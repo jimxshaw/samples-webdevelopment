57c6f5a110f8ac217639feece54732a018f3c5a1
Jim Shaw <jimxshaw@gmail.com>
2015-03-31 18:24:39 -0500

    initial reaction tester commit

--- reaction-tester/js/script.js
+++ reaction-tester/js/script.js
@@ -1,4 +1,22 @@
-document.alert("This is only a test!");
+var myBox = document.getElementById('box');
+
+function makeBox() {
+  var randomTime = Math.random();
+  randomTime = randomTime * 6000;
+  randomTime = Math.floor(randomTime);
+
+  setTimeout(function() {
+    myBox.style.display = "block";
+  }, randomTime);
+}
+
+myBox.onclick = function() {
+  myBox.style.display = "none";
+  makeBox();
+};
+
+makeBox();
+
 
 
 
