5a896e402d2e56f7808f986add65b5d6c9483ad4
Jim Shaw <jimxshaw@gmail.com>
2016-10-06 11:11:29 -0500

    Added js folder and index.js. Tested sample javascript.

---
+++ github-hub/js/index.js
@@ -0,0 +1,7 @@
+// index.js
+
+var message = "Hello JavaScript!";
+console.log(message);
+
+var resultsDiv = document.getElementById("results");
+resultsDiv.innerHTML = "<p>This is from JavaScript</p>";
