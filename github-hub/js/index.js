8ecafb4c868f82b8f77cff8304f6713c7bf1d1ea
Jim Shaw <jimxshaw@gmail.com>
2016-10-06 12:41:56 -0500

    Added jQuery cdn link from Google.

--- github-hub/js/index.js
+++ github-hub/js/index.js
@@ -1,7 +1,9 @@
 // index.js
 
-var message = "Hello JavaScript!";
-console.log(message);
+"use strict";
 
-var resultsDiv = document.getElementById("results");
-resultsDiv.innerHTML = "<p>This is from JavaScript</p>";
+
+
+
+
+
