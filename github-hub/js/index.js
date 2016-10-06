9a3d34e6845ee435f2229de2c3a621ecf6ab0250
Jim Shaw <jimxshaw@gmail.com>
2016-10-06 15:05:50 -0500

    Tested jQuery get method with actual github api data.

--- github-hub/js/index.js
+++ github-hub/js/index.js
@@ -3,6 +3,12 @@
 $(document).ready(function () {
     "use strict";
 
+    var gitHubSearch = "https://api.github.com/search/repositories?q=jquery+language:javascript&sort=stars";
+
+    $.get(gitHubSearch, function (result) {
+        displayResults(result.items);
+    });
+
     var resultList = $("#resultList");
 
     resultList.text("This is from jQuery");
@@ -19,6 +25,29 @@ $(document).ready(function () {
             toggleButton.text("Hide");
         }
     });
+
+    function displayResults(results) {
+        resultList.empty();
+
+        $.each(results, function (i, item) {
+            var newResult = $("<div class='result'>" +
+            "<div class='title'>" + item.name + "</div>" +
+            "<div>Language: " + item.language + "</div>" +
+            "<div>Owner: " + item.owner.login + "</div>" +
+            "</div>");
+
+            newResult.hover(function () {
+                // Make it darker
+                $(this).css("background", "lightgray");
+            }, 
+            function () {
+                // Reverse the color
+                $(this).css("background", "transparent");
+            });
+
+            resultList.append(newResult);
+        });
+    }
 
 });
 
